0773697934dc1bf67468a7e81460c05a499a337b
DongLongyu <dlylove286@gmail.com>
2024-11-19 22:28:06 +0800

    Initial commit

---
+++ lyblog-web/src/main/java/com/ly/LyblogWebApplication.java
@@ -0,0 +1,13 @@
+package com.ly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyblogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyblogWebApplication.class, args);
+	}
+
+}
